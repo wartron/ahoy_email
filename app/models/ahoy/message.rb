f320471c330b8e06346947cf22bfb06531c9aa3a
Will Wharton <willmwharton@gmail.com>
2017-01-21 22:31:53 -0800

    mail

--- app/models/ahoy/message.rb
+++ app/models/ahoy/message.rb
@@ -4,5 +4,9 @@ module Ahoy
 
     belongs_to :user, AhoyEmail.belongs_to.merge(polymorphic: true)
     belongs_to :user_form, AhoyEmail.belongs_to.merge(polymorphic: true), :foreign_key => :user_form_id
+
+    def mail
+      Mail.new(self.content)
+    end
   end
 end
